8aae6c61d268f5af396cacf0be7130604dc5310c
JustinAronson <JustinAronson@users.noreply.github.com>
2021-06-25 00:19:04 -0400

    Changed Directory Path

---
+++ PeopleSearchApplicationTests/Controllers/SearchPeopleControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SearchApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchApi.Controllers.Tests
+{
+    [TestClass()]
+    public class SearchPeopleControllerTests
+    {
+        [TestMethod()]
+        public void GetSearchPersonTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace PeopleSearchApplicationTests.Controllers
+{
+    class SearchPeopleControllerTests
+    {
+    }
+}
